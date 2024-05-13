13153e71d9becca2c8f0d68066622635bd4aaac4
chirprush <chirpyboat73@gmail.com>
2024-05-13 19:24:22 -0400

    Looking into convergence rates

--- gradient-ascent-eigenvectors/main.py
+++ gradient-ascent-eigenvectors/main.py
@@ -1,3 +1,4 @@
+from math import log, sqrt
 import numpy as np
 
 # A = np.array([
@@ -6,7 +7,7 @@ import numpy as np
 #     [0.99, 0.32, 0.62]
 # ])
 A = np.random.rand(400, 400)
-print(A)
+# print(A)
 
 n = A.shape[0]
 
@@ -64,17 +65,41 @@ def gradient(v):
     return grad
 
 # Wow that matches up really nicely yay
-print("Numerical gradient: ", numerical_gradient(g0))
-print("Gradient: ", gradient(g0))
+# print("Numerical gradient: ", numerical_gradient(g0))
+# print("Gradient: ", gradient(g0))
 
 g = g0
-gamma = 0.05
+gamma = 0.1
+
+scores = []
 
 for i in range(100):
     ascent = g + gamma * gradient(g)
     g = ascent / np.linalg.norm(ascent)
+    score = eigenind(g)
 
-    print(f"Step {i + 1} score: {eigenind(g)}")
+    scores.append(score)
+
+    print(f"Step {i + 1} score: {score}")
 
 print("Eigenvector:", g)
 print("Eigenvalue:", np.linalg.norm(np.dot(A, g)) / np.linalg.norm(g))
+
+# Using https://en.wikipedia.org/wiki/Rate_of_convergence#Order_estimation,
+# we may determine an approximation for the convergence rate of the algorithm
+
+# Tbh I'm not quite sure how accurate this is in terms of actually giving the
+# convergence rate (I might have to look at the actual math later) but it gives
+# that the convergence rate is roughly linear (although like some of the terms
+# are far greater or lower)
+"""
+estimates = [
+    log(abs((scores[k] - scores[k-1]) / (scores[k-1] - scores[k-2]))) / log(abs((scores[k-1] - scores[k-2]) / (scores[k-2] - scores[k-3])))
+    for k in range(3, len(scores))
+]
+
+q = sum(estimates) / len(estimates)
+
+print("Estimate convergence rates:", estimates)
+print("Approximate convergence rate:", q)
+"""
